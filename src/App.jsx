37eb68779097cc0e5795a0ed56faacc8b7def07c
Anushka <anushkagilda7@gmail.com>
2025-10-09 20:43:55 +0530

    Commit from Anushka

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddProject from "./pages/AddProject";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 import AllProject from "./pages/AllProject";
 
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/add-project" element={<AddProject />} />
           <Route path="/all-project" element={<AddProject />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
